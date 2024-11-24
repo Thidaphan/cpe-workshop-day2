1f3d70dbd3ef7dedf0117813577cb3fb2886d4d9
Thidaphan <plangthidaphan@gmail.com>
2024-11-24 13:18:34 +0700

    1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./services/db");
+const db = require("./service/db");
 
 const app = express();
 
